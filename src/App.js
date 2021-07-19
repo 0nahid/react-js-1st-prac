514a344c57923bbd9045b08500c6f1894a08a6a6
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-07-20 01:52:43 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name: `Nahid`,
+    job: "Singer" 
+}
+var style = {
+  color:'red',
+  backgroundColor:'darkBlue'
+}
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Done by nahid <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h1 className="" style ={style} >My first React Web Application</h1>
+        <Person name="John" job="Dev"></Person>
+        <Person name="Doe" job="Designer"></Person>
+        <Person name="Smith" job="Developer"></Person>
       </header>
     </div>
   );
 }
 
+function Person(props){
+  const style ={
+    width:'500px',
+    margin:'10px auto',
+    border: '1px solid red'
+  }
+ return(
+    <div style={style}>
+    <h1>My Name : {props.name} </h1>
+    <p>My job : {props.job}</p>
+  </div>
+ )
+}
 export default App;
